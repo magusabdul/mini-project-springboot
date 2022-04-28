1783cdf7b5e639b8e8a095632e86ead7280200f8
magusabdul <magusabdul@gmail.com>
2022-04-28 20:00:17 +0700

    update base project

---
+++ src/main/java/com/magusabdul/miniprojectspringboot/MiniProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.magusabdul.miniprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectSpringbootApplication.class, args);
+	}
+
+}
